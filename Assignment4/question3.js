4ee5f2139da48bd3f2daa0646cdedd4b232d4497
Kushagra <162109764+kushagra-successive@users.noreply.github.com>
2024-03-11 10:18:21 +0530

    Update question3.js

--- Assignment4/question3.js
+++ Assignment4/question3.js
@@ -1,11 +1,11 @@
 //program to give count of variables in a string ("abcabcdabbcc" => "a3b4c4d1")
 
 let str = "abcabcdabbcc";
-let arr = Array(123).fill(0); //defining the size of array and .fill is use for gdefault value
+let arr = Array(123).fill(0); //defining the size of array and .fill is use for default value
 let ansString = "";
 
 for (let i = 0; i < str.length; i++) {
-  arr[str.charCodeAt(i)]++; //at ascii value of every characters inside arra,increment value from 0 to its frequency
+  arr[str.charCodeAt(i)]++; //at ascii value of every characters inside array,increment value from 0 to its frequency
 }
 
 for (let i in arr) {
